be40755f305515fd20f502b1dd6b88947d25d9ce
wmps <wmps@gft.com>
2023-02-03 11:58:03 -0300

    Serialização YAML

--- SpringCourse/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ SpringCourse/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,7 +3,6 @@ package br.com.erudio.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonServices;
+import br.com.erudio.util.MediaType;
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -27,37 +27,49 @@ public class PersonController {
 	//private PersonServices service = new PersonServices();
 	
 	
-	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, 
-			MediaType.APPLICATION_XML_VALUE} )
+	@GetMapping(produces = {MediaType.APPLICATION_JSON, 
+			MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML})
 	public List<PersonVO> findAll(){
 		return service.findAll();
 	}
 	
 	@GetMapping(value = "/{id}",
-			produces = {MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE})
+			produces = {MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML})
 	public PersonVO findById(
 			@PathVariable(value = "id") Long id) throws Exception {
 		return service.findById(id) ;
 	}
 	
-	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,
-			MediaType.APPLICATION_XML_VALUE},
-			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON,
+			MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML},
+			produces = {MediaType.APPLICATION_JSON, 
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML})
 	public PersonVO create(@RequestBody PersonVO person) throws Exception {
 		return service.create(person);
 	}
 	
-	@PostMapping(value = "/v2", consumes = {MediaType.APPLICATION_JSON_VALUE, 
-			MediaType.APPLICATION_XML_VALUE},
-			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@PostMapping(value = "/v2", consumes = {MediaType.APPLICATION_JSON, 
+			MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML},
+			produces = {MediaType.APPLICATION_JSON, 
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML})
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) throws Exception {
 		return service.createV2(person);
 	}
 	
 	@PutMapping(
-			consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+			consumes = {MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML},
+			produces = {MediaType.APPLICATION_JSON,
+					MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML})
 	public PersonVO update(@RequestBody PersonVO person) throws Exception {
 		return service.update(person);
 	}
